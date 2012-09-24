407d5ba4f01d662b02b32953090f819b4ef6f06d
David Linsin <dlinsin@gmail.com>
2012-09-24 15:43:37 +0200

    Supporting more fields #5

--- mantis_issues_ext.safariextension/injected.js
+++ mantis_issues_ext.safariextension/injected.js
@@ -2,8 +2,8 @@
   Mantis related functions
  */
  
-var dataFields_de = ["Zusammenfassung", "Beschreibung", "Zusätzliche Information", "Rechnertyp", "Betriebssystem", "BS-Version", "Produktversion", "Zielversion"];
-var dataFields_en = ["Summary", "Description", "Additional Information", "Platform", "OS", "OS Version", "Product Version", "Target Version"];
+var dataFields_de = ["Zusammenfassung", "Beschreibung", "Zusätzliche Information", "Rechnertyp", "Betriebssystem", "BS-Version", "Produktversion", "Zielversion", "Reproduzierbar", "Priorität", "Auswirkung"];
+var dataFields_en = ["Summary", "Description", "Additional Information", "Platform", "OS", "OS Version", "Product Version", "Target Version", "Reproducibility", "Priority", "Severity"];
 var ctrl;
 var t;
 var s;
@@ -90,13 +90,13 @@ function findMantisData() {
 	addOverlay();
 	var summary = findCol(dataFields_de[0]);
 	if (summary != "") {
-		data = [summary, findCol(dataFields_de[1]), findCol(dataFields_de[2]), findCol(dataFields_de[3]), findCol(dataFields_de[4]), findCol(dataFields_de[5]), findCol(dataFields_de[6]), findCol(dataFields_de[7])];
+		data = [summary, findCol(dataFields_de[1]), findCol(dataFields_de[2]), findCol(dataFields_de[3]), findCol(dataFields_de[4]), findCol(dataFields_de[5]), findCol(dataFields_de[6]), findCol(dataFields_de[7]), findCol(dataFields_de[8]), findCol(dataFields_de[9]), findCol(dataFields_de[10])];
 		safari.self.tab.dispatchMessage("mantisData",data);
 		return;
 	}  
 	summary = findCol(dataFields_en[0]);
 	if (summary != "") {
-		data = [summary, findCol(dataFields_en[1]), findCol(dataFields_en[2]), findCol(dataFields_en[3]), findCol(dataFields_en[4]), findCol(dataFields_en[5]), findCol(dataFields_en[6]), findCol(dataFields_en[7])];
+		data = [summary, findCol(dataFields_en[1]), findCol(dataFields_en[2]), findCol(dataFields_en[3]), findCol(dataFields_en[4]), findCol(dataFields_en[5]), findCol(dataFields_en[6]), findCol(dataFields_en[7]), findCol(dataFields_en[8]), findCol(dataFields_en[9]), findCol(dataFields_en[10])];
 		safari.self.tab.dispatchMessage("mantisData",data);
 		return;
 	}
@@ -109,7 +109,7 @@ function findMantisData() {
 
 // inserts the data passed in the array into the "new github issue" page 
 // data_array: 0 = title, 1 = description, 2 = additional info, 3 = Platform, 4 = OS, 5 = OS Version, 
-//             6 = Product Version, 7 = Target Version, 8 = ID, 9 = Mantis Issue URL
+//             6 = Product Version, 7 = Target Version, 8 = Reproducibility, 9 = Priority, 10 = Severity, 11 = ID, 12 = Mantis Issue URL
 function insertMantisData(data_array) {
 	document.getElementById("issue_title").focus();
 	// set title
@@ -122,14 +122,20 @@ function insertMantisData(data_array) {
     var os_version_with_blockquote = "\n\n> _OS Version_: " + data_array[5];
     var product_version_with_blockquote = "\n\n> _Product Version_: " + data_array[6];    
     var target_version_with_blockquote = "\n\n> _Target Version_: " + data_array[7];                
+    var reproducibility_with_blockquote = "\n\n> _Reproducibility_: " + data_array[8];                
+    var priority_with_blockquote = "\n\n> _Priority_: " + data_array[9];
+    var severity_with_blockquote = "\n\n> _Severity_: " + data_array[10];                
 	var body = "\n\n\n\n_Mantis Info_:\n\n>" + desc_with_blockquote + "\n\n>" + 
 				additional_info_with_blockquote + 
 				platform_with_blockquote + 
 				os_with_blockquote + 
 				os_version_with_blockquote +				
 				product_version_with_blockquote +  
-				target_version_with_blockquote + 
-				"\n\n>[ID: " + data_array[8] + "](" + data_array[9] + ")";
+				target_version_with_blockquote +
+				reproducibility_with_blockquote +
+				priority_with_blockquote +
+				severity_with_blockquote + 
+				"\n\n>[ID: " + data_array[11] + "](" + data_array[12] + ")";
 	document.getElementsByTagName("textarea")[0].value = body;	
 }
 
